910b159cd57291d38de90f97423018eba05ca933
devpola <sonamoo9771@gmail.com>
2024-06-02 17:51:25 +0900

    feat(global): set material-ui environment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.Fragment>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </React.Fragment>
+);
